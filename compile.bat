9e29e20b96919d5f6910733a4e0a0f2d1ea4c3ec
leo <leo@berkut-tech.kz>
2018-10-03 04:31:33 +0600

    Copy fonts directory to result dir

--- compile.bat
+++ compile.bat
@@ -6,7 +6,7 @@ pyinstaller -y move.py
 
 mkdir dist\result
 
-xcopy /s /e /h dist\run dist\result
+xcopy /s /e /h /y dist\run dist\result
 xcopy /s /e /h /y dist\usb2 dist\result\
 move dist\result\usb2.exe dist\result\usb.exe
 xcopy /s /e /h /y dist\move dist\result\
@@ -18,8 +18,10 @@ copy move_history_tmp.p dist\result
 copy screen.ini dist\result\
 
 mkdir dist\result\pics
-xcopy /s /e /h pics dist\result\pics
+xcopy /s /e /h /y pics dist\result\pics
 mkdir dist\result\sprites_1380
-xcopy /s /e /h sprites_1380 dist\result\sprites_1380
+xcopy /s /e /h /y sprites_1380 dist\result\sprites_1380
 mkdir dist\result\sprites_1920
-xcopy /s /e /h sprites_1920 dist\result\sprites_1920
+xcopy /s /e /h /y sprites_1920 dist\result\sprites_1920
+mkdir dist\result\fonts
+xcopy /s /e /h /y fonts dist\result\fonts
