8c32ee6c338e8ee7f02eda70568db12298c4cdcf
leo <leo@berkut-tech.kz>
2018-10-03 04:04:06 +0600

    Converted TABs to spaces. No functional change

--- move.py
+++ move.py
@@ -1,36 +1,37 @@
-from pystockfish import *
 import cPickle as pickle
-import os, time
+import os
+import time
+
+from pystockfish import *
 
 params = {
-        "Contempt Factor": 0,
-        "Min Split Depth": 0,
-        "Threads": 2,
-        "Hash": 128,
-        "MultiPV": 1,
-        "Skill Level": 20,
-        "Move Overhead": 30,
-        "Minimum Thinking Time": 20, 
-        "Slow Mover": 80
-}  
+    "Contempt Factor": 0,
+    "Min Split Depth": 0,
+    "Threads": 2,
+    "Hash": 128,
+    "MultiPV": 1,
+    "Skill Level": 20,
+    "Move Overhead": 30,
+    "Minimum Thinking Time": 20,
+    "Slow Mover": 80
+}
 
-
-#def get_move( q, move_history, difficulty ):
- #   global game, best_move
+# def get_move( q, move_history, difficulty ):
+#   global game, best_move
 #    game = Engine(depth=difficulty, param=params)
 
-#pickle.dump( favorite_color, open( "save.p", "wb" ) )
-f = open( "move_history_tmp.p", "rb" )
-move_history, difficulty = pickle.load( f )
+# pickle.dump( favorite_color, open( "save.p", "wb" ) )
+f = open("move_history_tmp.p", "rb")
+move_history, difficulty = pickle.load(f)
 f.close()
 
-#game = Engine(depth=difficulty, param=params)
+# game = Engine(depth=difficulty, param=params)
 game = Engine(depth=difficulty)
 
-game.setposition( move_history )
+game.setposition(move_history)
 best_move = game.bestmove()
-f = open( "bestmove.p", "wb" )
-pickle.dump( best_move, f )
+f = open("bestmove.p", "wb")
+pickle.dump(best_move, f)
 f.flush()
 os.fsync(f)
 
