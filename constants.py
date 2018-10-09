f361fec43ced90fa823ff6cb9faec027889ae327
leo <leo@berkut-tech.kz>
2018-10-10 05:29:22 +0600

    Remove local_constants.py import as it is not needed anymore

--- constants.py
+++ constants.py
@@ -2,9 +2,3 @@ import os
 
 
 ENGINE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'engines')
-DEFAULT_ENGINE = 'stockfish'
-
-try:
-    from local_constants import *
-except ImportError:
-    pass
