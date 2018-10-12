cba6226d4f7e387566f0506b64a48f7b19705ab3
leo <leo@berkut-tech.kz>
2018-10-13 03:58:03 +0600

    Fix

--- run.py
+++ run.py
@@ -687,7 +687,12 @@ while 1:
                                 if DEBUG:
                                     print("Place pieces on their places")
                                 banner_right_places = True
-                                banner_place_pieces = True
+                                if not human_game:
+                                    white_to_move = len(move_history) % 2 == 0
+                                    if play_white == white_to_move:
+                                        banner_place_pieces = True
+                                else:
+                                    banner_place_pieces = True
                         else:
                             if DEBUG:
                                 print("All pieces on right places")
