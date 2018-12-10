7d44da3f261c59e358f5e5d9174acbd5f6361ad4
leotrubach <leotrubach@gmail.com>
2018-12-11 01:46:53 +0600

    Use usbtool for leds tool

--- leds.py
+++ leds.py
@@ -1,6 +1,8 @@
 import argparse
-import serial
-from utils import find_port
+import time
+import subprocess
+from utils import find_port, port2udp, port2number
+from socket import AF_INET, SOCK_DGRAM, socket, SOL_SOCKET, SO_REUSEADDR, SO_BROADCAST
 
 
 def parse_args():
@@ -16,15 +18,32 @@ def main():
         portname = find_port()
     else:
         portname = args.port
-    port = serial.Serial(portname, 38400, timeout=2.5)
-    try:
-        if args.all:
-            if args.all == 'on':
-                port.write('\xff' * 8)
-            elif args.all == 'off':
-                port.write('\x00' * 8)
-    finally:
-        port.close()
+    port = port2number(portname)
+    board_listen_port, gui_listen_port = port2udp(port)
+
+    SEND_SOCKET = ("127.0.0.1", board_listen_port)  # send to
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+    usb_command = ["python", "usbtool.py"]
+    if portname is not None:
+        usb_command.extend(["--port", portname])
+    usb_proc = subprocess.Popen(usb_command)
+    time.sleep(1)
+
+    message = None
+    if args.all:
+        if args.all == 'on':
+            message = '\xff' * 8
+        elif args.all == 'off':
+            message = '\x00' * 8
+
+    if message:
+        sock.sendto(message, SEND_SOCKET)
+
+    usb_proc.terminate()
 
 
 if __name__ == '__main__':
