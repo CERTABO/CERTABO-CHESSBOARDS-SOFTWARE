50a189ac1f3ab7492567b80899e485d83cb3f599
leotrubach <leotrubach@gmail.com>
2018-12-26 00:34:53 +0600

    Copy screen.ini too

--- compile.sh
+++ compile.sh
@@ -10,6 +10,7 @@ mkdir dist/result
 cp -r dist/run/* dist/result/
 cp -r dist/usbtool/* dist/result/
 
+cp screen.ini dist/result/
 cp -r pics dist/result/
 cp -r sprites_1380 dist/result/
 cp -r sprites_1920 dist/result/
