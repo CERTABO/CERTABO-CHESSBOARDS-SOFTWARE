be1051351907ce83366c64a353557f08b9b75e12
leotrubach <leotrubach@gmail.com>
2018-12-26 01:31:17 +0600

    Copy run.sh

--- compile.sh
+++ compile.sh
@@ -12,6 +12,9 @@ cp -r dist/usbtool/* dist/result/
 
 cp screen.ini dist/result/
 cp /usr/lib/python2.7/_sysconfigdata.py dist/result
+cp run.sh dist/result
+chmod +x dist/result/run.sh
+
 cp -r pics dist/result/
 cp -r sprites dist/result/
 cp -r sprites_1380 dist/result/
