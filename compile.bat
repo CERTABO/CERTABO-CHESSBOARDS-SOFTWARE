ca3cb8ef6962c6e7ad4b3cce56badf7e279ad23d
pmandurino <pitman@tiscali.it>
2019-12-08 20:42:27 +0100

    update version 3.3

--- compile.bat
+++ compile.bat
@@ -14,6 +14,8 @@ copy screen.ini dist\result\
 
 mkdir dist\result\pics
 xcopy /s /e /h /y pics dist\result\pics
+mkdir dist\result\sprites
+xcopy /s /e /h /y sprites dist\result\sprites
 mkdir dist\result\sprites_1380
 xcopy /s /e /h /y sprites_1380 dist\result\sprites_1380
 mkdir dist\result\sprites_1920
@@ -22,3 +24,9 @@ mkdir dist\result\fonts
 xcopy /s /e /h /y fonts dist\result\fonts
 mkdir dist\result\engines
 xcopy /s /e /h /y engines dist\result\engines
+mkdir dist\result\books
+xcopy /s /e /h /y books dist\result\books
+mkdir dist\result\sounds
+xcopy /s /e /h /y sounds dist\result\sounds
+mkdir dist\result\big_pgns
+xcopy /s /e /h /y big_pgns dist\result\big_pgns
